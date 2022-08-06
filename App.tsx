9ed3d850c298b3445c9299c0511257daa9e3f7ba
정석우 <ingal777@naver.com>
2022-08-06 16:13:29 +0900

    feat: signin,signup page + refactor: js->tsx

--- App.tsx
+++ App.tsx
@@ -7,196 +7,28 @@
  *
  * @format
  */
- import React, {type PropsWithChildren} from 'react';
- import {
-   Button,
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
- 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
+import React from 'react';
 
- import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
- 
- import CustomButton from './screen/CustomButton';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import InitialPage from './screen/InitialPage';
+import SignIn from './screen/SignIn';
+import SignUp from './screen/SignUp';
 
- import InitialPage from './screen/InitialPage';
+const Stack = createStackNavigator();
 
- import Login from './screen/Login';
-
- import Signin from './screen/Signin';
- 
- const Stack = createStackNavigator();
- 
- function App() {
- 
-   return (
-          <NavigationContainer>
-            <Stack.Navigator screenOptions={{headerShown : false}} initialRouteName="InitialPage">
-            <Stack.Screen name="InitialPage" component={InitialPage} />
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Signin" component={Signin} />
-            </Stack.Navigator>
-          </NavigationContainer>
-   );
- };
-
- 
- const styles = StyleSheet.create({
-   container:{
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-   },
-   text:{
-    color:"#3E382F",
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginTop:90,
-    marginBottom:60,
-   },
-   socialLoginTitle:{
-     color: '#B0A69D',
-     fontSize: 14,
-     marginTop: 100,
-     marginBottom: 20,
-   },
-   socialLoginIcons:{
-    justifyContent: "center",
-    flex:1,
-    flexDirection:"row",
-   }
- });
- 
- export default App;
-/*
-import React, {type PropsWithChildren} from 'react';
-import {
-  Button,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{headerShown: false}}
+        initialRouteName="InitialPage">
+        <Stack.Screen name="InitialPage" component={InitialPage} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="">
-          <Text>어플을 처음 이용하시나요?</Text>
-          <Button title="회원 가입" color="#4D483D" onPress={()=>{
-
-          }}/>
-          </Section>
-          <Section title="">
-          <Text>계정을 이미 등록하셨나요?</Text>
-          <Button title="로그인" color="#4D483D" onPress={()=>{
-
-          }}/>
-          </Section>
-          <Section title="">
-            <Text style={styles.socialLoginTitle}>소셜 로그인</Text>
-          </Section>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-    justifyContent: 'center',
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  button: {
-    backgroundColor: '#4D483D',
-  },
-  socialLoginTitle:{
-    color: '#B0A69D',
-    fontSize: 14,
-  }
-});
-
+}
 export default App;
-*/
---
+++ components/CustomButton.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {Component} from 'react';
+import {TouchableOpacity, Text, StyleSheet, Image} from 'react-native';
+
+interface CustomButtonProps {
+  title: string;
+  onPress: () => void;
+}
+
+function CustomButton({title, onPress}: CustomButtonProps) {
+  return (
+    <TouchableOpacity style={styles.textButton} onPress={onPress}>
+      <Text style={styles.buttonTitle}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  textButton: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#4D483D',
+    width: 207,
+    height: 56,
+    borderRadius: 100,
+  },
+  buttonTitle: {
+    color: 'white',
+    fontSize: 18,
+  },
+  socialIconButton: {
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+});
+
+export default CustomButton;
