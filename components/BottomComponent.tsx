f5b15021e5e47d11f483c569aa445421cf2635f9
Mina Park <mineezm9@gmail.com>
2022-09-11 04:36:27 +0900

    feat: 스케줄 더보기버튼 멤버목록확인(멤버전용) 개발

--- components/BottomComponent.tsx
+++ components/BottomComponent.tsx
@@ -7,66 +7,78 @@ import {TouchableOpacity, View,Text, StyleSheet, Image, Alert,  TextInput, Butto
 import {Card, Menu, Provider as PaperProvider} from 'react-native-paper';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/Store';
 
 interface BottomComponentProps{
   selectedScheduleId: any;
   bottomModalType: any;
 }
 function BottomComponent({selectedScheduleId, bottomModalType}:BottomComponentProps) {
-  //
-  //지출요약
-  //멤버목록_호스트
+  const accessToken = useSelector(
+    (state: RootState) => state.persist.user.accessToken,
+  );
+  const [approvedMemberInfo, setApprovedMemberInfo] = useState([]);
+
+  const getApprovedMember = async () => {
+    try{
+      const params ={
+        schedule_id: selectedScheduleId
+      };
+    
+      const headers ={
+        Authorization : `Bearer ${accessToken}`
+      }
+      
+
+      axios.get("http://10.0.2.2:8002/participants", {params,headers})
+      .then(res=>setApprovedMemberInfo(res.data))
+      .catch(err=>console.log(err));
+    } catch (err){
+      console.log(err);
+    }
+  }
+  
   if(bottomModalType === '지출요약'){
-
-
   return (
     <View>
-      <Text>
-    {
-      selectedScheduleId
-    }, {
-      bottomModalType
-    }
-      </Text>
-
+       {
+            approvedMemberInfo.map((item: any) =>{
+              
+              if(item != null)
+                return(
+                  <View>
+                    <Text>{item.participant_id}</Text>
+                    <Text>--------------------------</Text>
+                  </View>
+                )
+            })
+            
+        }
     </View>
   );
   }else if(bottomModalType==='멤버목록_멤버'){
-    const [info, setInfo] = useState([]);
     useEffect(() => {
-    AsyncStorage.getItem('user_id', (err, result1) => { //user_id에 담긴 아이디 불러오기
-
-      
-      result1 = '4008b5cb-c626-4a3a-9490-08572249ccf4'; //test0 계정
-      const params ={
-        schedule_id : selectedScheduleId
-      };
-    
-      //엑세스토큰 먼저 확인하고 id 가져오기 - 추후수정
-      //엑세스 토큰 만료되면 refresh로 액세스토큰 만들어주기
-      AsyncStorage.getItem('accessToken', (err, result2) => {
-        result2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQwMDhiNWNiLWM2MjYtNGEzYS05NDkwLTA4NTcyMjQ5Y2NmNCIsIm5hbWUiOiLthYzsiqTtirjsnKDsoIAwIiwiaWF0IjoxNjYyMjEyNDI3LCJleHAiOjE2NjM0MjIwMjd9.WOdLOl4a2WRCzqobCuuCLj5HE9fCtcGb2c2NUFZxHLA';
-        const headers ={
-        Authorization : `Bearer ${result2}`
-      }
-      
-      axios.get("http://10.0.2.2:8002/participants", {params,headers})
-      .then(res=>setInfo(res.data))
-      .catch(err=>console.log('3 : ',err));
+      getApprovedMember();
     });
-  });
-},[]);
-    return (
 
-      <View>
-      <Text>
-    {
-      JSON.stringify(info)
-    }
-      </Text>
-
+  return (
+    <View>
+       {
+            approvedMemberInfo.map((item: any) =>{
+              
+              if(item != null)
+                return(
+                  <View>
+                    <Text>{item.participant_id}</Text>
+                    <Text>--------------------------</Text>
+                  </View>
+                )
+            })
+            
+        }
     </View>
-    );
+  );
   } else{
     return (
       <View>
