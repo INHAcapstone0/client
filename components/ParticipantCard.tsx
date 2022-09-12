593f0b5952adff8c32fd6796788d95830df6dd1a
정석우 <ingal777@naver.com>
2022-09-12 16:39:05 +0900

    fix: eslint error

--- components/ParticipantCard.tsx
+++ components/ParticipantCard.tsx
@@ -1,41 +1,55 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, {Component, useRef, useState} from 'react';
-import {TouchableOpacity,View, Text, StyleSheet, Image,Dimensions, Alert, Button } from 'react-native';
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Dimensions,
+  Alert,
+  Button,
+} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faCrown, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faCrown, faEllipsisV} from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
-import {Card, Button as PaperButton, Menu as PaperMenu, Provider as PaperProvider} from 'react-native-paper';
-import { Menu, MenuItem, MenuDivider } from 'react-native-material-menu';
+import {
+  Card,
+  Button as PaperButton,
+  Menu as PaperMenu,
+  Provider as PaperProvider,
+} from 'react-native-paper';
+import {Menu, MenuItem, MenuDivider} from 'react-native-material-menu';
 import BottomSheet from 'reanimated-bottom-sheet';
 
-interface ParticipantCardProps{
+interface ParticipantCardProps {
   item: any;
-
 }
-function ParticipantCard({item}:ParticipantCardProps) {
-    return (
+function ParticipantCard({item}: ParticipantCardProps) {
+  return (
     <View style={styles.card}>
-        <Text style={styles.userName}>
-            <Image style={styles.userImage} source={{ uri: item.User.img_url,}}/>  {item.User.name}
-            </Text>
+      <Text style={styles.userName}>
+        <Image style={styles.userImage} source={{uri: item.User.img_url}} />{' '}
+        {item.User.name}
+      </Text>
     </View>
-    );
+  );
 }
 
 const styles = StyleSheet.create({
-  card:{
-    marginLeft:20
+  card: {
+    marginLeft: 20,
   },
-  userName:{
+  userName: {
     height: 60,
-    color: "#4D483D",
-    fontFamily: 'Jalnan'
+    color: '#4D483D',
+    fontFamily: 'Jalnan',
   },
-  userImage:{
+  userImage: {
     width: 40,
-    height:40
-  }
+    height: 40,
+  },
 });
 
 export default ParticipantCard;
--- components/ReceiptCard.tsx
+++ components/ReceiptCard.tsx
@@ -1,77 +1,88 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, {Component, useEffect, useRef, useState} from 'react';
-import {TouchableOpacity,View, Text, StyleSheet, Image,Dimensions, Alert, Button } from 'react-native';
-import { faCircleQuestion, IconDefinition } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Dimensions,
+  Alert,
+  Button,
+} from 'react-native';
+import {
+  faCircleQuestion,
+  IconDefinition,
+} from '@fortawesome/free-regular-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 
-interface ReceiptCardProps{
+interface ReceiptCardProps {
   item: any;
-
 }
-function ReceiptCard({item}:ReceiptCardProps) {
-    const [payDate, setPayDate] = useState('');
-    const [iconType, setIconType] = useState();
+function ReceiptCard({item}: ReceiptCardProps) {
+  const [payDate, setPayDate] = useState('');
+  const [iconType, setIconType] = useState();
 
-    useEffect(() => {
-        setPayDate(item.payDate.substring(0, 10));
-    }, []);
+  useEffect(() => {
+    setPayDate(item.payDate.substring(0, 10));
+  }, []);
 
-    
-    if(item.category ==='기타'){
+  if (item.category === '기타') {
     return (
-    <View style={styles.card}>
-        <FontAwesomeIcon icon={faCircleQuestion} style={styles.icon} size={ 35 }/>
+      <View style={styles.card}>
+        <FontAwesomeIcon
+          icon={faCircleQuestion}
+          style={styles.icon}
+          size={35}
+        />
         <View style={styles.content}>
-        <Text style={styles.place}>{item.place_of_payment}</Text>
+          <Text style={styles.place}>{item.place_of_payment}</Text>
 
-        <View style={styles.detailContent}>
+          <View style={styles.detailContent}>
             <Text style={styles.date}>{payDate}</Text>
             <Text style={styles.price}>{item.total_price} 원</Text>
+          </View>
         </View>
-        </View>
-    </View>
+      </View>
     );
-    }
-    else{
-        return(
-        <View></View>
-        );
-    }
+  } else {
+    return <View></View>;
+  }
 }
 
 const styles = StyleSheet.create({
-    card:{
-        marginLeft: 20,
-        marginTop: 10,
-        marginBottom: 10,
-        width: Dimensions.get('window').width*0.8, 
-        flexDirection: "row",
-    },
-    content:{
-        marginLeft: 10
-    },
-    detailContent:{
-        width: Dimensions.get('window').width*0.7,
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    icon:{
-       marginTop : 7,
-       color: '#4D483D'
-    },
-    place:{
-        fontSize: 18,
-        color: '#4D483D',
-        fontFamily: 'Jalnan'
-    },
-    date:{
-        color: '#4D483D',
-        fontFamily: 'Jalnan'
-    },
-    price:{
-        color: '#4D483D',
-        fontFamily: 'Jalnan'
-    }
+  card: {
+    marginLeft: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    width: Dimensions.get('window').width * 0.8,
+    flexDirection: 'row',
+  },
+  content: {
+    marginLeft: 10,
+  },
+  detailContent: {
+    width: Dimensions.get('window').width * 0.7,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  icon: {
+    marginTop: 7,
+    color: '#4D483D',
+  },
+  place: {
+    fontSize: 18,
+    color: '#4D483D',
+    fontFamily: 'Jalnan',
+  },
+  date: {
+    color: '#4D483D',
+    fontFamily: 'Jalnan',
+  },
+  price: {
+    color: '#4D483D',
+    fontFamily: 'Jalnan',
+  },
 });
 
 export default ReceiptCard;
